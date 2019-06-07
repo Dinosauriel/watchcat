7f2767a982ee98368ba8153df25aac5cf0d82561
Aurel Feer <aurel.feer@gmail.com>
2019-06-07 15:01:18 +0200

    upgraded to .yaml file

--- watchcat.py
+++ watchcat.py
@@ -1,27 +1,51 @@
+#!/usr/bin/env python
 
 import subprocess
 import re
 import os.path
+import yaml
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
+
+
 
 
 root = os.path.dirname(__file__)
 
-servers = open(os.path.join(root, "servers.txt"))
+servers_yaml = open(os.path.join(root, "servers.yaml"))
+servers = yaml.load(servers_yaml, Loader=Loader)
+servers_yaml.close()
+
 
 screens_active = subprocess.run(["screen", "-ls"], stdout=subprocess.PIPE).stdout.decode("utf-8")
-
+i = -1
 for server in servers:
-	server_name = server.strip()
-
-	if server_name[0:2] == "//" or server_name[0] == "#" or server_name == "":
-		#this is a commment
-		continue
+	i += 1
+	#print(server)
 	
+	if not server["enabled"]:
+		continue;
+
+	server_name = server["name"]
+
+	if server_name is None:
+		print("please provide a name for server number " + str(i))
+		continue
+
+
 	if re.search(server_name, screens_active) is not None:
 		#this server is running
+		#print("server " + server_name + " is already running")
 		continue
 
-	print("server " + server_name + " is not running. starting...")
-	subprocess.run(["sh", os.path.join(root, "../start-" + server_name + ".sh")], stdout=subprocess.DEVNULL)
 
-servers.close()
+	server_file = server["file"]
+	if server_file is None:
+		print("please provide a start file for the server " + server_name)
+		continue
+
+
+	print("server " + server_name + " is not running. running file: " + server_file)
+	subprocess.run(["sh", server_file], stdout=subprocess.DEVNULL)
